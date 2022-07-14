2fc9a5ac34d844efa214aeaf6a7709ea4ec17cb6
nnayzawkhant <nnayzawkhant@gmail.com>
2022-07-14 20:10:25 +0630

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 var Icon = document.querySelector(".icon")
 var Navber = document.querySelector(".navber")
 
+
+
 Icon.onclick = function(){
-    
+    Navber.classList.toggle(".small-navber")
+    Navber.style.display = "block";
+    this.style.display = "none";
+    Icon.style.display = "block";
 }
+Icon.onclick = function(){
+    Navber.style.display = "block"
+}
+
+
